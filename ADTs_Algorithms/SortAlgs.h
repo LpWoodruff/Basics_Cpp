4f0565e09274ad183a92215ed6d5af5de7b1d7d0
Lincon Woodruff <lp.woodruff@outlook.com>
2024-05-19 00:56:46 -0700

    Added O(n^2) sort algorithms

--- ADTs_Algorithms/SortAlgs.h
+++ ADTs_Algorithms/SortAlgs.h
@@ -1,7 +1,66 @@
 #pragma once
+#include <iostream>
+#include <vector>
 class SortAlgs
 {
 	private:
-		int num = 0;
-};
+		std::vector<int> nums;
+		int n;
 
+	public:
+		std::vector<int> get_nums() {
+			return nums;
+		}
+
+	// Constructor
+		SortAlgs(std::vector<int>& numbers) : nums(numbers), n(numbers.size()) {}
+
+
+
+
+	// Selection Sort
+	/*
+		Selection sort works by selecting an element in an array, and compares it to all other elements linearly.
+	*/
+		void ss_Sort()
+		{
+			for (int i = 0; i < n - 1; i++)
+			{
+				int smallest = i;
+				for (int j = i + 1; j < n; j++)
+				{
+					if (nums[j] < nums[smallest])
+					{
+						smallest = j;
+					}
+				}
+				if (smallest != i)
+				{
+					std::swap(nums[smallest], nums[i]);
+				}
+			}
+		}
+
+
+	// Insertion Sort
+	/*
+		Insertion sort works by dividing an array into two parts, and sorts them individually	
+	*/
+		void is_Sort()
+		{
+			int key, j;
+			for (int i = 1; i < n; i++)
+			{
+				key = nums[i];
+				j = i - 1;
+				while (j >= 0 && nums[j] > key)
+				{
+					nums[j + 1] = nums[j];
+					j = j - 1;
+				}
+				nums[j + 1] = key;
+			}
+		}
+	 
+
+};
--- ADTs_Algorithms/main.cpp
+++ ADTs_Algorithms/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "SortAlgs.h"
 
 int main()
 {
-	std::cout << "Hello World";
+	std::vector<int> testCase = { 2,59,24,12,11,3,5,16,29,30, 0,42,32, };
+	int size = testCase.size();
+	
+	SortAlgs sorter(testCase);
+
+
+	sorter.is_Sort();
+
+	std::vector<int> sorted = sorter.get_nums();
+
+;
+
+	for (int i = 0; i < sorted.size(); i++) 
+	{
+		std::cout << sorted[i];
+		if (i < sorted.size() - 1)
+		{
+			std::cout << ", ";
+		}
+	}
+
+	
 }
